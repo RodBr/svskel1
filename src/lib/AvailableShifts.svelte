32dadd7bc7bfa2117101f39d2932b94ed87b4f31
RodBr <rod.brown@live.com>
2023-11-08 17:08:39 +1100

    accordion

---
+++ src/lib/AvailableShifts.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+
+	let shiftTexts = [
+		{
+			line1: 'Open Shift Available',
+			line2: 'Start Mon 11/11 at 9am - End Tue 12/11 at 2am',
+			line3: 'HTR-GV1 Lock Box 1187'
+		},
+		{
+			line1: 'Open Shift Available2',
+			line2: 'Start Mon 11/11 at 9am - End Tue 12/11 at 2am',
+			line3: 'HTR-GV1 Lock Box 1187'
+		},
+		{
+			line1: 'Open Shift Available3',
+			line2: 'Start Mon 11/11 at 9am - End Tue 12/11 at 2am',
+			line3: 'HTR-GV1 Lock Box 1187'
+		},
+		{
+			line1: 'Open Shift Available4',
+			line2: 'Start Mon 11/11 at 9am - End Tue 12/11 at 2am',
+			line3: 'HTR-GV1 Lock Box 1187'
+		}
+	];
+</script>
+
+<div class="responsive-container !max-h-80 !overflow-y-scroll">
+	<div class="card p-4 bg-gradient-to-br variant-gradient-primary-secondary">
+		<Accordion>
+			{#each shiftTexts as row, i}
+				<AccordionItem open>
+					<svelte:fragment slot="lead">😃</svelte:fragment>
+					<svelte:fragment slot="summary"><h2>{row.line1}</h2></svelte:fragment>
+					<svelte:fragment slot="content">
+						{row.line2}
+						<br />{row.line3}
+					</svelte:fragment>
+				</AccordionItem>
+			{/each}
+
+			<!-- ... -->
+		</Accordion>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
@@ -14,25 +14,9 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Prospect Hill</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
 				<a
 					class="btn btn-sm variant-ghost-surface"
 					href="https://github.com/skeletonlabs/skeleton"
@@ -41,6 +25,7 @@
 				>
 					GitHub
 				</a>
+				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
